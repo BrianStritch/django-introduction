cecaf743dc9f99cb2faf4e5443160f029eaa9f62
BrianStritch <brians12ca18@gmail.com>
2022-02-19 23:37:01 +0000

    generate report for coverage and fix testing to get 100%

--- todo/test_models.py
+++ todo/test_models.py
@@ -5,5 +5,9 @@ from .models import Item
 class TestModels(TestCase):
 
     def test_done_default_to_false(self):
-        item = Item.objects.create(name= 'test todo item')
+        item = Item.objects.create(name='test todo item')
         self.assertFalse(item.done)
+
+    def test_item_string_method_returns_name(self):
+        item = Item.objects.create(name='test todo item')
+        self.assertEqual(str(item), 'test todo item')
